76219ccbd5c5ec51f67c58b3c5cd45bfbf2fb1e8
BaltazarAyalew <baltazarseife123@gmail.com>
2024-03-22 20:06:01 +0100

    inital commmit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-3w0zoit97kq2.frontegg.com',
+  clientId: 'c0c9062a-bf16-426f-934f-3b3400f5e7b5'
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
